f21937e5c19c1d45116d7f2e290366b91a3da14b
Nayla Gomes <nayla.cgs@gmail.com>
2021-01-28 13:16:51 -0300

    Node - conceitos e testes

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+// Query Params = ?users=1
+// Route Params = /users/1
+// Request Body = { "name": "Nayla", "email": "nayla@gmail.com" }
+
+// CRUD = Create, Read, Update, Delete
+
+const users = ['Nayla', 'Felipe', 'Jônatas'];
+
+// Middleware global de log
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+// Middleware local
+function checkUserExists(req, res, next) {
+    if (!req.body.name)
+        return res.status(400).json({ error: 'User name is required' });
+
+    return next();
+}
+
+// Middleware local
+function checkUserInArray(req, res, next) {
+    const user = users[req.params.index];
+
+    if(!user) 
+        return res.status(400).json({ error: 'User does not exists' });
+
+    req.user = user;
+
+    return next();
+}
+
+server.get('/users', (req, res) => {
+    return res.json(users);
+});
+
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    // const { index }  = req.params;
+    // return res.json(users[index]);
+
+    return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
+    const { name } = req.body;
+
+    users.push(name);
+
+    return res.json(users);
+});
+
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res) => {
+    const { name } = req.body;
+    const { index } = req.params;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+server.delete('/users/:index', checkUserInArray, (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    
+    return res.json(users);
+});
+
+server.listen(3000);
